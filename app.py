3b474d011f3aee64c7a6ced0958284bd980d039c
aspireboy001 <darshanshekharpatil@gmail.com>
2023-12-13 20:57:42 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,114 @@
+from flask import Flask, render_template, request, Response
+import cv2
+import numpy as np
+from utils import *
+from flask import send_file
+import os
+
+
+app = Flask(__name__)
+
+UPLOAD_FOLDER = 'static/temp'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+camera_url = 'http://192.168.43.82:8080/shot.jpg'
+path = "./static/images/uploaded_image.jpg"
+
+scale = 3
+hp = 210 * scale
+wp = 297 * scale
+
+cap = cv2.VideoCapture(0)
+if not cap.isOpened():
+    print("Error: Could not open camera.")
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+def detect_dimensions(frame):
+    print("Detecting dimensions...")
+    imgContour, fc = getContours(frame, minArea=50000, filter=4, draw=True)
+
+    if len(fc) != 0:
+        biggestCont = fc[0][2]
+        cv2.drawContours(imgContour, biggestCont, -1, (0, 0, 255), 3)
+        imgWarp = warpImg(frame, biggestCont, wp, hp)
+
+        imgContour2, fc2 = getContours(imgWarp, minArea=2000, filter=4, threshold=[50, 50], draw=False)
+
+        if len(fc2) != 0:
+            for obj in fc2:
+                cv2.polylines(imgContour2, [obj[2]], True, (0, 255, 0), 2)
+                npoints = reorder(obj[2])
+                newW = round((findDis(npoints[0][0] // scale, npoints[1][0] // scale) / 10), 1)
+                newH = round((findDis(npoints[0][0] // scale, npoints[2][0] // scale) / 10), 1)
+                cv2.arrowedLine(imgContour2, (npoints[0][0][0], npoints[0][0][1]),
+                                (npoints[1][0][0], npoints[1][0][1]), (255, 0, 255), 3, 8, 0, 0.05)
+                cv2.arrowedLine(imgContour2, (npoints[0][0][0], npoints[0][0][1]),
+                                (npoints[2][0][0], npoints[2][0][1]), (255, 0, 255), 3, 8, 0, 0.05)
+                x, y, w, h = obj[3]
+                cv2.putText(imgContour2, '{}cm'.format(newW), (x + 30, y - 10), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1.5,
+                            (255, 0, 255), 2)
+                cv2.putText(imgContour2, '{}cm'.format(newH), (x - 70, y + h // 2), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1.5,
+                            (255, 0, 255), 2)
+            _, buffer = cv2.imencode('.jpg', imgContour2)
+            return buffer.tobytes()
+
+    # If no contours are detected, return an empty byte string
+    print("No contours detected...")
+    return b''
+
+
+
+def gen():
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+        frame = cv2.resize(frame, (0, 0), None, 0.5, 0.5)
+        frame_bytes = detect_dimensions(frame)
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame_bytes + b'\r\n\r\n')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+@app.route('/upload', methods=['POST'])
+def upload_image():
+    print("Here - upload_image")
+
+    if 'file' not in request.files:
+        return render_template('index.html', error="No file part")
+
+    file = request.files['file']
+
+    if file.filename == '':
+        return render_template('index.html', error="No selected file")
+
+    if file:
+        file.save(path)
+        img = cv2.imread(path)
+        print("Image loaded")
+        frame_bytes = detect_dimensions(img)
+
+        print(f"Length of frame_bytes: {len(frame_bytes)}")
+
+        if frame_bytes:
+            print("Dimensions detected")
+            temp_path = os.path.join(app.config['UPLOAD_FOLDER'], 'processed_image.jpg')
+            cv2.imwrite(temp_path, cv2.imdecode(np.frombuffer(frame_bytes, np.uint8), cv2.IMREAD_COLOR))
+            # return render_template('index.html', image_path=path, image_bytes=frame_bytes)
+            return send_file(temp_path, mimetype='image/jpeg')
+        else:
+            print("No dimensions detected")
+            return render_template('index.html', error="No dimensions detected")
+
+    return render_template('index.html', error="Unexpected error")
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ utils.py
@@ -0,0 +1,66 @@
+import cv2
+import numpy as np
+
+def getContours(img,threshold=[100,100],showCanny=False,minArea=1000,filter=0,draw=False):
+    imgGray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    imgBlur=cv2.GaussianBlur(imgGray,(5,5),1)
+    imgCanny=cv2.Canny(imgBlur,threshold[0],threshold[1])
+    kernel=np.ones((5,5))
+    imgDial=cv2.dilate(imgCanny,kernel,iterations=3)
+    imgThresh=cv2.erode(imgDial,kernel,iterations=2)
+    imgRes=cv2.resize(imgThresh,(0,0),None,0.5,0.5)
+    if showCanny: cv2.imshow('Canny Image',imgRes)
+    
+    contours,hierarchy=cv2.findContours(imgThresh,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+
+    finalContours=[]
+    for i in contours:
+        area=cv2.contourArea(i)
+        if area>minArea:
+            peri=cv2.arcLength(i,True)
+            approx=cv2.approxPolyDP(i,0.02*peri,True)
+            boundingbox=cv2.boundingRect(approx)
+            if filter > 0:
+                if len(approx)==filter:
+                    finalContours.append([len(approx),area,approx,boundingbox,i])
+            else:
+                finalContours.append([len(approx),area,approx,boundingbox,i])
+            
+    finalContours=sorted(finalContours,key=lambda x:x[1],reverse=True)
+    
+    if draw:
+        for con in finalContours:
+            cv2.drawContours(img,con[4],-1,(0,0,255),3)
+            
+    return img,finalContours
+
+
+def reorder(points):
+    # print(points.shape)
+    newpoints= np.zeros_like(points)
+    points=points.reshape((4,2))
+    add=points.sum(1)
+    newpoints[0]=points[np.argmin(add)]
+    newpoints[3]=points[np.argmax(add)]
+    diff=np.diff(points,axis=1)
+    newpoints[1]=points[np.argmin(diff)]
+    newpoints[2]=points[np.argmax(diff)]
+    return newpoints
+
+
+def warpImg(img,points,width,height,pad=20):
+    # print(points)
+    points=reorder(points)
+    
+    pts1=np.float32(points)
+    pts2=np.float32([[0,0],[width,0],[0,height],[width,height]])
+    matrix=cv2.getPerspectiveTransform(pts1,pts2)
+    imgWarp=cv2.warpPerspective(img,matrix,(width,height))
+    imgWarp=imgWarp[pad:imgWarp.shape[0]-pad,pad:imgWarp.shape[1]-pad]
+    
+    return imgWarp
+
+def findDis(pts1,pts2):
+    return ((pts2[0]-pts1[0])**2 + (pts2[1]-pts1[1])**2)**0.5
+
+
